a88f6850c5cfc72282e562b4272c5a1773250226
Anil Kumar <anilkrmvazure@gmail.com>
2026-05-08 10:38:07 +0000

    final

--- main.tf
+++ main.tf
@@ -26,9 +26,9 @@ terraform {
 }
 
 module "securestorage" {
-  source              = "app.terraform.io/anilkrmv/securestorage/azurerm"
-  version             = "1.0.0"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  source               = "app.terraform.io/anilkrmv/securestorage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
   storage_account_name = "anilkrmvoragelab12ee3"
 }
